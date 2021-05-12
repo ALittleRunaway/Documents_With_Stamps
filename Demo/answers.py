a9a707a4897b1767e78aac17c3057025ff8ad29c
Maria Petrova <MaryCool674@gmail.com>
2021-05-12 17:28:09 +0300

    Errors changed to Russian

--- Demo/answers.py
+++ Demo/answers.py
@@ -1,4 +1,5 @@
 """Class for retuening answers in json format"""
+import re
 
 class AnswerDictionaries():
     """Makes json dictionaris for answer"""
@@ -6,37 +7,39 @@ class AnswerDictionaries():
     @staticmethod
     def no_error_answer():
         """When everything is fine"""
-        d = {'error_info': {}}
-        d['error_info'] = {"error_code": 0, "error_name": None,
-                           "error_message": "Your file has been saved successfully"}
+        d = {'информация_об_ошибке': {}}
+        d['информация_об_ошибке'] = {"код_ошибки": 0, "название_ошибки": None,
+                                     "сообщение_ошибки": "Ваш файл был успешно сохранён"}
         return d
 
     @staticmethod
     def pypdf2_errors(e):
         """For the sqlalchemy errors"""
-        d = {'error_info': {}}
+        d = {'информация_об_ошибке': {}}
 
         if str(e) == "EOF marker not found":
-            d['error_info']['error_code'] = 1
-            d['error_info']['error_name'] = str(e)
-            d['error_info']['error_message'] = "There is no file or it is not in the right format"
+            d['информация_об_ошибке']['код_ошибки'] = 1
+            d['информация_об_ошибке']['название_ошибки'] = "Неверная ссылка на файл"
+            d['информация_об_ошибке']['сообщение_ошибки'] = "Переданного файла не существует или он в неверном формате"
 
         return d
 
     @staticmethod
     def existing_errors(e):
         """For the sqlalchemy errors"""
-        d = {'error_info': {}}
+        d = {'информация_об_ошибке': {}}
 
         if str(e).startswith("[Errno 2]"):
-            d['error_info']['error_code'] = 2
-            d['error_info']['error_name'] = "FileNotFoundError"
-            d['error_info']['error_message'] = str(e)
+            d['информация_об_ошибке']['код_ошибки'] = 2
+            d['информация_об_ошибке']['название_ошибки'] = "Файл не найден"
+            pattern = r'(?<=directory: ).+(?=)'
+            message = "Нет такого файла или директории: " + re.findall(pattern, str(e))[0]
+            d['информация_об_ошибке']['сообщение_ошибки'] = message
 
         elif str(e).startswith("Invalid URL"):
-            d['error_info']['error_code'] = 3
-            d['error_info']['error_name'] = "InvalidURLError"
-            d['error_info']['error_message'] = "No schema supplied. Try changing your url"
+            d['информация_об_ошибке']['код_ошибки'] = 3
+            d['информация_об_ошибке']['название_ошибки'] = "Неверная ссылка"
+            d['информация_об_ошибке']['сообщение_ошибки'] = "Ошибка в запросе. Попробуйте изменить url"
 
         return d
 
