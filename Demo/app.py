bdda7b3e40fd51ce9f8038e9de777bf5eb821329
Maria Petrova <MaryCool674@gmail.com>
2021-05-16 00:39:04 +0300

    Half of the logs is done

--- Demo/app.py
+++ Demo/app.py
@@ -5,17 +5,36 @@ from push_pull_file import PushPullFile
 from answers import AnswerDictionaries
 from PyPDF2 import utils
 from requests import exceptions
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False # To work with utf-8
 app.config['JSON_SORT_KEYS'] = False # To save the right order in the dictionaries
 
+
 @app.route("/", methods=['GET'])
 @app.route("/home", methods=['GET'])
 def index():
     """Shows the empty main page"""
-    return render_template("index.html")
+    errors = request.args.get('errors')
+    if errors is None:
+        # make logs reversed
+        with open("reversed_logs.log", "w") as f:
+            for line in reversed(list(open("logs.log"))):
+                color_1 = line.find("[37m") - 1
+                color_2 = line.find("[0m") - 1
+                if color_1 < 0 or color_2 < 0:
+                    pass
+                else:
+                    line = line[:color_1] + line[color_1 + 5:color_2] + line[color_2 + 4:]
+                f.write(line.rstrip() + "\n")
+        # take uor ready logs and dislay them
+        with open("reversed_logs.log", "r") as f:
+            logs = f.readlines()
+        return render_template("index.html", logs=logs)
+    return ":C"
 
 
 @app.route("/stamp", methods=['GET'])
@@ -27,8 +46,6 @@ def stamp():
     arguments = {"take_path": take_path,
                  "put_path": put_path,
                  }
-    # Для открытия окна загрузки раскомментировапть строчку ниже
-    # return send_file(path, as_attachment=True)
     # handling errors
     try:
         path = PushPullFile.pull_file(arguments)
@@ -44,7 +61,14 @@ def stamp():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='logs.log', level=logging.DEBUG, filemode="w")
+    # root_logger = logging.getLogger()
+    # root_logger.setLevel(logging.DEBUG)  # or whatever
+    # handler = logging.FileHandler('logs.log', 'w', 'utf-8')  # or whatever
+    # root_logger.addHandler(handler)
+
     app.run(host='127.0.0.1', debug=True)
+
 
 
 
